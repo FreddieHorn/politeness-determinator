b476022b3d438abd16ea699ce8b7fdc18d2432f9
mohamad7395 <mohammad.akbarimonfared@gmail.com>
2023-06-30 15:33:37 +0200

    cleaned title_body column added

--- main.py
+++ main.py
@@ -14,7 +14,12 @@ if __name__ == "__main__":
 
     dataset = pd.read_csv(file)
     dataset = dataset[dataset['comment_body'] != '[deleted]']#deleting deleted comments from the dataset since they are useless
+    dataset = dataset[dataset['post_title'] != '[deleted]']
+    dataset = dataset[dataset['post_title'] != '[deleted by user]']
+
+    dataset['title_body'] = dataset['comment_body']+ ' ' + dataset['post_title']
     dataset['clean_text'] = dataset['comment_body'].apply(lambda x: data_preprocessor.process(x))
+    dataset['clean_title_body'] = dataset['title_body'].apply(lambda x: data_preprocessor.process(x))
     
     #SPLITTING THE TRAINING DATASET INTO TRAIN AND TEST
     X_train, X_test, y_train, y_test = train_test_split(dataset["clean_text"], dataset['offensiveness_score'],test_size=0.2,shuffle=True)
